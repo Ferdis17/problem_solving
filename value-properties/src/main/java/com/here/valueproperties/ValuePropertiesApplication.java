a39b37bb91df6e4637db3708e0a6856c978238fa
Ferdis17 <fmuriyesu@gmail.com>
2020-02-07 00:43:14 -0800

    reading values from property file

---
+++ value-properties/src/main/java/com/here/valueproperties/ValuePropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.here.valueproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValuePropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValuePropertiesApplication.class, args);
+	}
+
+}
---
+++ value-properties/src/test/java/com/here/valueproperties/ValuePropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.here.valueproperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValuePropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
